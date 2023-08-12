062784796f9294ea37d5af108241b9ccd2dcc66b
NateKenopic <nkenopic@outlook.com>
2023-08-11 21:14:31 -0400

    Django and React Notes App

---
+++ project1/frontend/src/components/App.jsx
@@ -0,0 +1,16 @@
+import Note from "./Notes";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+
+  return (
+    <div className='App'>
+        <Header />
+        <Note />
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
